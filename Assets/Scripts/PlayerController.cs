978d270ec305eef2ce4844868587232913265113
Pi <ines.f.ribeiro12@gmail.com>
2026-03-23 23:38:22 +0000

    Implement object collisions for your 2D game

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,17 +6,24 @@ using UnityEngine.InputSystem;
 public class PlayerController : MonoBehaviour
 {
     public InputAction MoveAction;
+    Rigidbody2D rigidbody2d;
+    Vector2 move;
     // Start is called before the first frame update
     void Start()
     {
         MoveAction.Enable();
+        rigidbody2d = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        Vector2 move = MoveAction.ReadValue<Vector2>();
+        move = MoveAction.ReadValue<Vector2>();
         Debug.Log(move);
+    }
+
+    void FixedUpdate()
+    {
         Vector2 position = (Vector2)transform.position + move * 3.0f * Time.deltaTime;
         transform.position = position;
     }
